dc70d89f3189706ba1b51adafb6a968fbcedabea
Jesus Santiago <jesusantiago420@gmail.com>
2023-01-26 16:51:06 -0300

    diseño terminado mobile - JavaScript funcionando hasta la segunda pagina

--- assents/script.js
+++ assents/script.js
@@ -5,35 +5,38 @@ const n3 = document.querySelector("[data-tipo='n3']");
 const n4 = document.querySelector("[data-tipo='n4']");
 const n5 = document.querySelector("[data-tipo='n5']");
 const button = document.querySelector("[data-tipo='button']");
-
-const divgracias = document.querySelector("[data-tipo='gracias']");
+const tarjeta2 = document.querySelector("[data-tipo='tarjeta2']");
+const cambio = document.getElementById("cambio")
 const segundapagina = document.querySelector("[data-tipo='calificacion]");
+const img = document.getElementById("img");
+const usuarioInput = document.querySelector("[data-tipo='calificacionImput']")
 
 
+const valor = [];
 
 const select1 = n1.innerHTML;
 const select2 = n2.innerHTML;
 const select3 = n3.innerHTML;
 const select4 = n4.innerHTML;
 const select5 = n5.innerHTML;
+const problema = tarjeta2.innerHTML;
 
-const textoGracias = divgracias;
 
 
-// const gracias = () => {
-//     alert("Hola")
-//     textoGracias.classList.add("div-central-gracias");
-// }
+const gracias = () => {
+    
+    const cambio1 = cambio.classList.replace("adios", "div-central-gracias")
+    const cambioImg = img.classList. replace("adios", "img-celular")
+    usuarioInput.textContent = "You selected 4 out of 5";
+}
 
 const calificacion = () => {
     central.style.display = "none";
+    gracias();
 }
 
 const juego = () =>{
-    // gracias();
     calificacion();
-
-    alert(textoGracias)    
 }
 
     n1.addEventListener("click", juego);
@@ -41,6 +44,5 @@ const juego = () =>{
     n3.addEventListener("click", juego);
     n4.addEventListener("click", juego);
     n5.addEventListener("click", juego);
-
-
-    button.addEventListener("click", juego);
+    
+button.addEventListener("click", juego);
