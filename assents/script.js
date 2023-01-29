178e5f99b840a420397e616461069cb7baba6543
Jesus Santiago <jesusantiago420@gmail.com>
2023-01-28 22:05:17 -0300

    JavaScript listo hasta el titulo, falta implementar la logica de la funcion imprimirTitulo. HTML y CSS mobile listo

--- assents/script.js
+++ assents/script.js
@@ -1,48 +1,63 @@
-const central = document.querySelector("[data-tipo='div-central']");
+
+const padreImput = document.querySelector("[data-tipo='PadreImput']");
+// const usuarioInput = document.querySelector("[data-tipo='calificacionImput']");
 const n1 = document.querySelector("[data-tipo='n1']");
 const n2 = document.querySelector("[data-tipo='n2']");
 const n3 = document.querySelector("[data-tipo='n3']");
 const n4 = document.querySelector("[data-tipo='n4']");
 const n5 = document.querySelector("[data-tipo='n5']");
-const button = document.querySelector("[data-tipo='button']");
-const tarjeta2 = document.querySelector("[data-tipo='tarjeta2']");
-const cambio = document.getElementById("cambio")
-const segundapagina = document.querySelector("[data-tipo='calificacion]");
-const img = document.getElementById("img");
-const usuarioInput = document.querySelector("[data-tipo='calificacionImput']")
 
+// const valorDigitado = [select1, select2, select3, select4, select5];
 
-const valor = [];
+// for(let i = 0; i < valorDigitado.length, i++) {
+    
+
+// }
 
 const select1 = n1.innerHTML;
 const select2 = n2.innerHTML;
 const select3 = n3.innerHTML;
 const select4 = n4.innerHTML;
 const select5 = n5.innerHTML;
-const problema = tarjeta2.innerHTML;
+
+const valorObtenido = document.createElement("h1");
+valorObtenido.classList.add("calificacion-titulo")
+valorObtenido.textContent = ("You select " + select1 + " of 5.");
+
+padreImput.appendChild(valorObtenido);
 
 
+
+
+n1.addEventListener("click", valorIngresado);
+n2.addEventListener("click", valorIngresado);
+n3.addEventListener("click", valorIngresado);
+n4.addEventListener("click", valorIngresado);
+n5.addEventListener("click", valorIngresado);
+
+function impirmirTitulo1 () {
+    
+} 
+
+const central = document.querySelector("[data-tipo='div-central']");
+const button = document.querySelector("[data-tipo='button']");
+const tarjeta2 = document.querySelector("[data-tipo='tarjeta2']");
+const cambio = document.getElementById("cambio")
+const segundapagina = document.querySelector("[data-tipo='calificacion]");
+const img = document.getElementById("img");
+
+    const problema = tarjeta2.innerHTML;
 
 const gracias = () => {
     
     const cambio1 = cambio.classList.replace("adios", "div-central-gracias")
     const cambioImg = img.classList. replace("adios", "img-celular")
-    usuarioInput.textContent = "You selected 4 out of 5";
-}
+   }
 
 const calificacion = () => {
     central.style.display = "none";
     gracias();
+    console.log(select1)
 }
-
-const juego = () =>{
-    calificacion();
-}
-
-    n1.addEventListener("click", juego);
-    n2.addEventListener("click", juego);
-    n3.addEventListener("click", juego);
-    n4.addEventListener("click", juego);
-    n5.addEventListener("click", juego);
     
-button.addEventListener("click", juego);
+button.addEventListener("click", calificacion);
